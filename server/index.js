153a1fb7c9e8d8be8990f6f9318229e458902f4d
aqilsulthan <aqilsulthaan@gmail.com>
2023-03-20 23:07:11 +0700

    edit client package

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ const port = 3000;
 
 app.use(cors());
 
+app.use((req, res, next) => { 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/", router);
 
 app.listen(port, () => {
