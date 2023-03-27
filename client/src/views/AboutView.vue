d9f89ad1987ebf5e31e5107c0e11047f4c40161e
Annisa Rahmapuri <annisa.rahmapuri@gmail.com>
2023-03-27 18:48:39 +0700

    about

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,363 +1,211 @@
 <template>
-  <div>
-    <div class="container text-center mb-3 pb-3">
-      <h1>Tekmul woy</h1>
+  <div class="background-image">
+    <!-- konten komponen Vue.js Anda -->
+
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <!-- <a class="navbar-brand " href="#">
+          <img src="../assets/logo.png" width="30" height="30" alt="Logo">
+          Cakara
+        </a> -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div class="about-container">
+    <!-- <h1>About Our Website</h1> -->
+    <img class="about-image" src="../assets/about.png" width="500" height="60"  alt="About Us Image">
+    <br>
+    <img class="about-image" src="../assets/cakara.png" alt="About Us Image">
+    <div >
+      <!-- <div class="col-lg-6">
+        <img class="about-image" src="../assets/foto_rara.jpg" alt="About Us Image">
+      </div> -->
+      <div class="col-lg-12">
+        <div class="logo-cakra">
+          <img class="about-image" src="../assets/logo.png" alt="About Us Image">
+        </div>
+      </div>
+      <div class="col-lg-12">
+        <p class="about-description">Membaca seringkali merupakan perjuangan berat bagi orang-orang dengan gangguan seperti disleksia, ADHD, dan ketidakmampuan visual atau belajar. Tidak sedikit pelajar yang sering merasa tertinggal karena perbedaan tersebut yang menyebabkan kurangnya kepercayaan diri. Belum lagi orang dewasa yang telah mengatasi kekurangannya tetapi masih merasa kesulitan membaca dan berimbas pada produktifitas kerja.</p>
+        <p class="about-description">Untungnya, kemajuan teknologi telah berevolusi. Dengan adanya teknologi text to speech (TTS), kita dapat mengasah kemampuan membaca dan meningkatkan produktivitas dalam segala bidang. Hal tersebut dapat terpenuhi dengan beberapa fitur pada aplikasi Hahahahah ini, yaitu text to speech, terjemahan berbagai bahasa, input file text document dari penyimpanan, input langsung melalui link website, serta fitur stop, pause, resume dan clear audio.</p>
+        <p class="about-description">Tujuan dan Manfaat dari aplikasi ini tidak tertuju pada suatu kalangan tertentu. Aplikasi ini dapat bermanfaat untuk kumpulan orang yang memiliki gangguan untuk membaca seperti pengidap disleksia maupun para anak kecil yang baru belajar membaca. Selain itu, aplikasi ini dapat meningkatkan produktivitas banyak golongan orang dengan dapat “membaca” apapun yang ingin kita baca sesuai dengan kebutuhan masing masing, seperti contoh berita terkini, resep masakan, maupun dokumen kerja yang belum sempat kita review.</p>
+      </div>
     </div>
-    <div class="container text-center mt-2 pt-2">
-      <div class="col">
-        <div class="card">
-          <div class="card-header">Text-to-speech</div>
-          <div class="card-body grid gap-0 column-gap-2 p-2 m-2 g-col-6">
-            <div class="input-group input-group-sm mb-3">
-              <button @click="convertLink()" class="input-group-text btn btn-primary">Convert to Text</button>
-              <input type="text" class="form-control" v-model="url" placeholder="Masukkan URL Website">
-            </div>
-            <textarea 
-              placeholder="Masukkan Text"
-              class="form-control" 
-              style="height: 500px;" 
-              v-model="textDefault">
-            </textarea>
-            <br>
-            <div class="d-flex gap-2 lang-select">
-              <button @click="speechCreate()" class="btn btn-primary">
-                Speak
-              </button>
-              <select class="form-select" 
-                aria-label="Default select example"
-                v-model="langOption"
-                >
-                <option selected disabled>Select a languange</option>
-                <option value="ID">Bahasa Indonesia</option>
-                <option value="EN">Bahasa Inggris</option>
-                <option value="JV">Bahasa Jawa</option>
-                <option value="FR">Bahasa Perancis</option>
-              </select>
-              <div class="doc-select">
-                <input 
-                  type="file"
-                  class="form-control"
-                  id="formFile"
-                  ref="doc"
-                  @change="readFile()">
-              </div>
-              <div class="d-flex flex-row">
-                <button @click="clearText()" class="btn btn-primary">
-                  Clear
-                </button>
-                <div>
-                  <button v-if="pause" @click="pauseSpeech()" class="btn btn-primary">
-                    Pause
-                  </button>
-                  <button v-else @click="resumeSpeech()" class="btn btn-primary">
-                    Resume
-                  </button>
-                </div>
-                <button @click="stopSpeech()" class="btn btn-primary">
-                  Stop
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-    <!-- Translate dari TTS -->
+    <br>
+    <div class="b">
+      <!-- <h1>Meet Our Team</h1> -->
+      <img class="about-image" src="../assets/meet.png" width="400" height="50" alt="About Us Image">
+    </div>
     <br><br>
-      <div class="col">
-        <div class="card w-100">
-          <div class="card-body">
-            <h5 class="card-title">Terjemahkan ke bahasa lain</h5>
-            <div class="form-floating pb-3">
-              <textarea 
-                class="form-control"
-                style="height: 100px"
-                v-model="afterTranslationDefault"
-                disabled
-              ></textarea>
-            </div>
-            <!-- select languange -->
-            <div class="d-flex flex-row w-60 gap-2">
-              <select
-                class="form-select"
-                v-model="translateToDefault"
-              >
-                <option value="indonesian">Bahasa Indonesia</option>
-                <option value="english">Bahasa Inggris</option>
-                <option value="javanese">Bahasa Jawa</option>
-                <option value="sundanese">Bahasa Sunda</option>
-              </select>
-              <div class="d-flex flex-row justify-content-end">
-                <a class="btn btn-primary" @click="translateTextDefault()">Translate</a>
-              </div>
-              <div class="d-flex flex-row justify-content-end">
-                <a class="btn btn-primary" @click="speakTranslatedTextDefault()">Speak</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Translate -->
-      <br /><br />
-
     <div class="row">
-      <div class="col">
-        <div class="card w-100">
-          <div class="card-body">
-            <h5 class="card-title">From</h5>
-            <div class="form-floating pb-3">
-                  <textarea
-                    placeholder="Masukkan Teks"
-                    class="form-control"
-                    id="floatingTextarea2"
-                    style="height: 100px"
-                    v-model="beforeTranslation"
-                  ></textarea>
-                </div>
-                <div class="d-flex flex-row gap-2 w-50">
-                  <select
-                      class="form-select"
-                      aria-label="Default select example"
-                      v-model="translateFrom"
-                    >
-                      <option value="indonesian">Bahasa Indonesia</option>
-                      <option value="english">Bahasa Inggris</option>
-                      <option value="javanese">Bahasa Jawa</option>
-                      <option value="sundanese">Bahasa Sunda</option>
-                    </select>
-                  </div>
+      <!-- <div class="col-md-4">
+          <div class="card">
+            <img class="card-img-top" src="../assets/foto_rara.jpg" alt="Team Member">
+            <div class="card-body">
+              <h5 class="card-title">John Doe</h5>
+              <p class="card-text">Team Member</p>
+            </div>
           </div>
+      </div> -->
+
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_aqil.jpg" alt="Team Member 1">
+          <h4>Aqil Sulthan Yuki Maye</h4>
+          <p>5027211007</p>
         </div>
       </div>
-  
-      <div class="col">
-        <div class="card w-100">
-          <div class="card-body">
-            <h5 class="card-title">To</h5>
-            <div class="form-floating pb-3">
-                  <textarea
-                    class="form-control"
-                    id="floatingTextarea2"
-                    style="height: 100px"
-                    v-model="afterTranslation"
-                    disabled
-                  ></textarea>
-                </div>
-                <!-- select language -->
-                <div class="d-flex flex-row w-60 gap-2">
-                  <select
-                      class="form-select"
-                      aria-label="Default select example"
-                      v-model="translateTo"
-                    >
-                      <option value="indonesian">Bahasa Indonesia</option>
-                      <option value="english">Bahasa Inggris</option>
-                      <option value="javanese">Bahasa Jawa</option>
-                      <option value="sundanese">Bahasa Sunda</option>
-                    </select>
-                    <div class="d-flex flex-row justify-content-end">
-                      <a class="btn btn-primary" @click="translateText()">Translate</a>
-                    </div>
-                    <div class="d-flex flex-row justify-content-end">
-                      <a class="btn btn-primary" @click="speakTranslatedText()">Speak</a>
-                    </div>
-                </div>
-          </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_rehan.jpg" alt="Team Member 2">
+          <h4>Athaya Reyhan Nugroho</h4>
+          <p>5027221067</p>
         </div>
       </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_defa.jpg" alt="Team Member 3">
+          <h4>Adimasdeftara Bimasena S.</h4>
+          <p>5027211040</p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_arkan.jpg" alt="Team Member 1">
+          <h4>Arkan Hendri Abdul G. B.</h4>
+          <p>5027211026</p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_diba.jpg" alt="Team Member 2">
+          <h4>Adiba Zalfa Camilla</h4>
+          <p>5027211060</p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_rara.jpg" alt="Team Member 3">
+          <h4>Annisa Rahmapuri</h4>
+          <p>5027211018</p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-member">
+          <img src="../assets/foto_poni.jpg" alt="Team Member 1">
+          <h4>M. Hilmy Adhi Pradityo</h4>
+          <p>5027211053</p>
+        </div>
+      </div> 
+      <div class="col-lg-12">
+        <br>
+        <p class="about-description">Website ini dibuat sebgai tugas 3 Konsep Dasar Multimedia dari Mata Kuliah Teknologi Multimedia</p>
+        <p class="about-description">Dosen Pengampu  Ibu Henning Titi Ciptaningtyas S.Kom, M.Kom</p>
+        </div>
     </div>
   </div>
-</div>
+ </div>
 </template>
 
-  
-  <style>
-    .background-image {
-      background-image: url('@/assets/BackgroundWeb.png');
-      background-size: cover;
-      background-position: center center;
-    }
-  </style>
-  
-  <script>
+<script>
 import axios from 'axios';
 import DOMPurify from 'dompurify';
-import Speech from "speak-tts";
-import Swal from 'sweetalert2';
-import translate from "translate";
 
 export default {
   data() {
     return {
       url: '',
-      textDefault: '',
-      text: "",
-      file: null,
-      langOption: null,
-      lang: null,
-      voiceName: null,
-      beforeTranslation: '',
-      translateFrom: '',
-      translateTo: '',
-      translateToDefault: '',
-      afterTranslation: '',
-      afterTranslationDefault: '',
-      translateVoice: '',
-      pause: true,
+      plainText: '',
     };
   },
   methods: {
     async convertLink() {
       try {
         const response = await axios.get(this.url);
-        const sanitizedHTML = 
-          DOMPurify.sanitize(response.data, 
-          { ALLOWED_TAGS: []});
-        this.textDefault = sanitizedHTML;
+        const sanitizedHTML = DOMPurify.sanitize(response.data, { KEEP_FORMATTING: true });
+        this.plainText = sanitizedHTML;
       } catch (error) {
-        Swal.fire({
-          icon: 'error',
-          title: 'Failed to Convert Website',
-          text: 'Sorry, but this website have a CORS policy'
-        })
         console.error(error);
       }
     },
-    async speechCreate() {
-      try {
-      let speech;
-      if (this.langOption == "ID") {
-        speech = new Speech();
-        this.lang = "id-ID";
-        this.voiceName = "Google Bahasa Indonesia";
-      } else if (this.langOption == "EN") {
-        speech = new Speech();
-        this.lang = "en-GB";
-        this.voiceName = "Google UK English Female";
-      } else if (this.langOption == "JV") {
-        speech = new Speech();
-        speech.setLanguage("jv-ID");
-        this.lang = "jv-ID";
-        this.voiceName = "Microsoft Siti Online (Natural) - Javanese (Indonesia)";
-      } else if (this.langOption == "FR") {
-        speech = new Speech();
-        speech.setLanguage("fr-FR");
-        this.lang = "fr-FR";
-        this.voiceName = "Microsoft Eloise Online (Natural) - French (France)";
-      }
-      await speech.init({
-        volume: 0.5,
-        lang: this.lang,
-        rate: 1,
-        pitch: 1,
-        name: this.voiceName,
-        voiceURI: this.voiceName,
-        splitSentences: true,
-        maxLength: 500,
-      });
-      speech.speak({
-        text: this.textDefault,
-      });
-    } catch(error) {
-      Swal.fire({
-          icon: 'error',
-          title: 'Bahasa belum dipilih',
-          text: 'Pilih bahasa terlebih dahulu'
-        })
-        console.error(error);
-    }
-    },
-    pauseSpeech() {
-      let speech = new Speech()
-      speech.pause()
-      this.pause = false
-    },
-    resumeSpeech() {
-      let speech = new Speech()
-      speech.resume()
-      this.pause = true
-    },
-    stopSpeech() {
-      let speech = new Speech()
-      speech.cancel()
-    },
-    readFile() {
-      this.file = this.$refs.doc.files[0];
-      const reader = new FileReader();
-      if (this.file.name.includes(".txt")) {
-        reader.onload = (res) => {
-          this.textDefault = res.target.result;
-        };
-        reader.onerror = (err) => console.log(err);
-        reader.readAsText(this.file);
-      } else {
-        this.textDefault = "check the console for file output";
-        reader.onload = (res) => {
-          console.log(res.target.result);
-        };
-        reader.onerror = (err) => console.log(err);
-        reader.readAsText(this.file);
-      }
-    },
-    clearText() {
-      this.textDefault = "";
-      this.afterTranslationDefault = "";
-    },
-    async translateText() {
-      const fromLang = this.translateFrom;
-      const toLang = this.translateTo;
-      // console.log(fromLang, toLang);
-      let text = await translate(this.beforeTranslation, {
-        from: fromLang,
-        to: toLang,
-      });
-      // console.log(text);
-      this.afterTranslation = text;
-    },
-    async speakTranslatedText(){
-      const speech = new Speech()
-      
-      if (this.translateTo == "indonesian") {
-        this.lang = "id-ID";
-        this.voiceName =
-          "Microsoft Ardi Online (Natural) - Indonesian (Indonesia) " ||
-          "Google Bahasa Indonesia";
-      } else if (this.translateTo == "english") {
-        this.lang = "en-GB";
-        this.voiceName =
-          "Google UK English Female" ||
-          "Microsoft Maisie Online (Natural) - English (United Kingdom)";
-      } else if (this.translateTo == "javanese") {
-        speech.setLanguage("jv-ID");
-        this.lang = "jv-ID";
-        this.voiceName =
-          "Microsoft Siti Online (Natural) - Javanese (Indonesia)";
-      } else if (this.translateTo == "sundanese") {
-        speech.setLanguage("su-ID");
-        this.lang = "su-ID";
-        this.voiceName =
-          "Microsoft Tuti Online (Natural) - Sundanese (Indonesia)";
-      }
-  
-      await speech.init({
-          volume: 0.5,
-          lang: this.lang,
-          rate: 1,
-          pitch: 1,
-          name: this.voiceName,
-          voiceURI: this.voiceName,
-          splitSentences: true,
-          listeners: {
-            onvoiceschanged: (voices) => {
-              console.log("Event voiceschanged", voices);
-            },
-          },
-        });
-        speech.speak({
-          text: this.afterTranslation,
-        });
-    }
   },
 };
 </script>
+
+
+<style>
+/* @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'); */
+
+  .background-image {
+    background-image: url('../assets/background2.png');
+    background-size: cover;
+    background-position: center center;
+    font-family: 'Poppins';
+    font-weight: 400;
+    font-size: 25px;
+    color: #3D366B;
+    
+  }
+  .about-container {
+  max-width: 1500px;
+  margin: auto;
+  padding: 40px;
+  text-align: center;
+}
+
+.navbar-nav {
+  display: flex;
+  justify-content: flex-end;
+  margin: 0 0 0 1100px;
+  font-weight: 600;
+  color: #3D366B;
+}
+
+.navbar-nav a {
+  color: #3D366B;
+}
+
+
+
+.logo-cakra img {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+
+.container-fluid img {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+
+.card {
+  width: 200px;
+}
+
+
+.team-member {
+  text-align: center;
+}
+
+.team-member img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+
+</style>
